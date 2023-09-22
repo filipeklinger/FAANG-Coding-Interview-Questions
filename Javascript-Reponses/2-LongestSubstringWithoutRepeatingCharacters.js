b9a68882d1dc18fb1280b6782c4f97a5390cba37
Filipe Klinger <filipe@tg4.com.br>
2023-09-22 17:28:52 -0300

    fix: for the last example

--- Javascript-Reponses/2-LongestSubstringWithoutRepeatingCharacters.js
+++ Javascript-Reponses/2-LongestSubstringWithoutRepeatingCharacters.js
@@ -23,29 +23,40 @@ Output: 3
 Explanation: The answer is "wke", with the length of 3.
 Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
 
+Input: s = "dvdf"
+Output: 3
+Explanation: The answer is "vdf", with the length of 3.
+
  */
 
 /**
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function(s) {
+var lengthOfLongestSubstring = function (s) {
     let biggerS = [];
     let currS = [];
-    for(i=0;i<s.length;i++){
+    for (i = 0; i < s.length; i++) {
         const c = s[i];
-        if(!currS.includes(c)){
+        if (!currS.includes(c)) {
             currS.push(c)
-        }else{
-            if(biggerS.length < currS.length){
-                biggerS = [...currS];
-            }
-            currS = [c];
+        } else {
+            //se o caractere ja existe no array, entao o array deve ser reiniciado
+            //a partir do caractere repetido
+            const index = currS.indexOf(c);
+            currS = [
+                ...currS.slice(index + 1),
+                c
+            ];
         }
-        console.log("currS",currS);
+        console.log("currS: ",currS);
+        //a cada iteracao, verifica se o tamanho do array atual eh maior que o maior
+        if (biggerS.length < currS.length) {
+            biggerS = [...currS];
+        }
     }
-    console.log("biggerS",biggerS);
-    return biggerS.length
+    
+    return biggerS.length;
 };
 
-console.log(lengthOfLongestSubstring("pwwkew"));
+console.log(lengthOfLongestSubstring("dvdf"));
